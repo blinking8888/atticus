a5cedc341fa80b20404486bccec23e38cadb7088
Nestor Aguilar <2113804+blinking8888@users.noreply.github.com>
2023-04-20 08:14:38 +0800

    Use `thiserror` for our Error type

--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,17 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use thiserror::Error;
+
+#[derive(Debug, Clone, Error, PartialEq, Eq)]
 /// The types of errors that this crate could indicate
 pub enum Error {
+    #[error("Problem in sending a Request to the Actor")]
     /// A problem was encounted during `send()`ing of the request
-    RequestError(String),
+    RequestError,
+
+    #[error("Problem in receiving a Response from the Actor")]
     /// An error was encounted while waiting for the response from the `Actor` instance.
-    ResponseError(String),
+    ResponseError,
+
+    #[error("Problem in sending an event to the Actor")]
     /// A problem was encounted in sending an event
-    EventError(String),
+    EventError,
 }
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use Error::*;
-
-        let display = match self {
-            RequestError(ref s) | ResponseError(ref s) | EventError(ref s) => s,
-        };
-        write!(f, "{}", display)
-    }
-}
-
-impl std::error::Error for Error {}
